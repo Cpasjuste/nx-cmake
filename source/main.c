d40ec7ae23d672db99237ffdc32e977c815b7327
cpasjuste <cpasjuste@gmail.com>
2018-03-21 11:44:45 +0100

    - working with held

--- source/main.c
+++ source/main.c
@@ -7,9 +7,57 @@
 #define SCREEN_WIDTH    1280
 #define SCREEN_HEIGHT   720
 
+typedef struct NXKey {
+    char *name;
+    int id;
+} NXKey;
+
+NXKey keys[27] =
+        {
+                {"KEY_A            ", 0},    ///< A
+                {"KEY_B            ", 1},    ///< B
+                {"KEY_X            ", 2},    ///< X
+                {"KEY_Y            ", 3},    ///< Y
+                {"KEY_LSTICK       ", 4},    ///< Left Stick Button
+                {"KEY_RSTICK       ", 5},    ///< Right Stick Button
+                {"KEY_L            ", 6},    ///< L
+                {"KEY_R            ", 7},    ///< R
+                {"KEY_ZL           ", 8},    ///< ZL
+                {"KEY_ZR           ", 9},    ///< ZR
+                {"KEY_PLUS         ", 10},   ///< Plus
+                {"KEY_MINUS        ", 11},   ///< Minus
+                {"KEY_DLEFT        ", 12},   ///< D-Pad Left
+                {"KEY_DUP          ", 13},   ///< D-Pad Up
+                {"KEY_DRIGHT       ", 14},   ///< D-Pad Right
+                {"KEY_DDOWN        ", 15},   ///< D-Pad Down
+                {"KEY_LSTICK_LEFT  ", 16},   ///< Left Stick Left
+                {"KEY_LSTICK_UP    ", 17},   ///< Left Stick Up
+                {"KEY_LSTICK_RIGHT ", 18},   ///< Left Stick Right
+                {"KEY_LSTICK_DOWN  ", 19},   ///< Left Stick Down
+                {"KEY_RSTICK_LEFT  ", 20},   ///< Right Stick Left
+                {"KEY_RSTICK_UP    ", 21},   ///< Right Stick Up
+                {"KEY_RSTICK_RIGHT ", 22},   ///< Right Stick Right
+                {"KEY_RSTICK_DOWN  ", 23},   ///< Right Stick Down
+                {"KEY_SL           ", 24},   ///< SL
+                {"KEY_SR           ", 25},   ///< SR
+                {"KEY_TOUCH       ",  26},
+
+        };
+
+const char *key_to_string(u64 key) {
+    for (int i = 0; i < 27; i++) {
+        if (BIT(keys[i].id) & key) {
+            return keys[i].name;
+        }
+    }
+    return "UNKNOW";
+}
+
+
 int main(int argc, char **argv) {
 
     u64 kDown = 0;
+    Result res = 0;
 
     gfxInitDefault();
     gfxSetMode(GfxMode_TiledDouble);
@@ -23,23 +71,25 @@ int main(int argc, char **argv) {
 
     printf("main loop\n");
 
-    hidSetNpadJoyAssignmentModeSingleByDefault(CONTROLLER_PLAYER_1);
-    hidSetNpadJoyAssignmentModeSingleByDefault(CONTROLLER_PLAYER_2);
+    res = hidSetNpadJoyAssignmentModeSingleByDefault(CONTROLLER_PLAYER_1);
+    printf("hidSetNpadJoyAssignmentModeSingleByDefault(P1): %x\n", res);
+    res = hidSetNpadJoyAssignmentModeSingleByDefault(CONTROLLER_PLAYER_2);
+    printf("hidSetNpadJoyAssignmentModeSingleByDefault(P2): %x\n", res);
 
     while (appletMainLoop()) {
 
         hidScanInput();
 
-        kDown = hidKeysDown(CONTROLLER_PLAYER_1);
+        kDown = hidKeysHeld(CONTROLLER_PLAYER_1);
         if (kDown) {
-            printf("P1: %lx\n", kDown);
+            printf("P1: %s\n", key_to_string(kDown));
             if (kDown & KEY_B)
                 break;
         }
 
-        kDown = hidKeysDown(CONTROLLER_PLAYER_2);
+        kDown = hidKeysHeld(CONTROLLER_PLAYER_2);
         if (kDown) {
-            printf("P2: %lx\n", kDown);
+            printf("P2: %s\n", key_to_string(kDown));
         }
 
         gfxFlushBuffers();
@@ -47,8 +97,12 @@ int main(int argc, char **argv) {
         gfxWaitForVsync();
     }
 
-    hidSetNpadJoyAssignmentModeDual(CONTROLLER_PLAYER_1);
-    hidSetNpadJoyAssignmentModeDual(CONTROLLER_PLAYER_2);
+    res = hidSetNpadJoyAssignmentModeDual(CONTROLLER_PLAYER_1);
+    printf("hidSetNpadJoyAssignmentModeDual(P1): %x\n", res);
+    res = hidSetNpadJoyAssignmentModeDual(CONTROLLER_PLAYER_2);
+    printf("hidSetNpadJoyAssignmentModeDual(P2): %x\n", res);
+
+    svcSleepThread(1000 * 1000 * 1000);
 
     gfxExit();
 #ifndef EMU
